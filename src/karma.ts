73f84a6740fb3c73530be136a972a26a98478532
BUDISANTOSO <budi.santoso@salt.co.id>
2024-09-15 14:32:09 +0700

    feat: inital module commit 0.0.1

---
+++ src/karma.ts
@@ -0,0 +1,106 @@
+import {
+  BuilderContext,
+  BuilderOutput,
+  createBuilder,
+} from "@angular-devkit/architect";
+import { JsonObject } from "@angular-devkit/core";
+import {
+  executeKarmaBuilder,
+  KarmaBuilderOptions,
+} from "@angular-devkit/build-angular";
+import { Configuration } from "webpack";
+import * as fs from "fs";
+import * as path from "path";
+import { cwd } from "process";
+import util from "util";
+import { Observable } from "rxjs";
+
+export type CustomBuilderSchema = JsonObject &
+  KarmaBuilderOptions & {
+    webpack?: string;
+  };
+
+export function customKarmaBuilder(
+  options: CustomBuilderSchema,
+  context: BuilderContext
+): Observable<BuilderOutput> {
+  context.logger.info("\nCustom builder is running...");
+
+  let configCallback: (
+    configuration: Configuration,
+    context: BuilderContext
+  ) => Configuration;
+
+  if (options.webpack) {
+    const webpackPath = path.resolve(context.workspaceRoot, options.webpack);
+
+    if (fs.existsSync(webpackPath)) {
+      try {
+        configCallback = require(webpackPath);
+        context.logger.info(
+          `\nLoaded custom Webpack configuration from: ${webpackPath}`
+        );
+      } catch (error) {
+        context.logger.error(
+          `\nError loading custom Webpack config at: ${webpackPath}`
+        );
+        throw error;
+      }
+    } else {
+      context.logger.error(
+        `\nCustom Webpack config file does not exist at: ${webpackPath}`
+      );
+      throw Error(
+        `\nCustom Webpack config file does not exist at: ${webpackPath}`
+      );
+    }
+  }
+
+  return executeKarmaBuilder(options, context, {
+    webpackConfiguration: (config: Configuration) => {
+      const snapshotDirectory = path.join(cwd(), ".webpack");
+
+      const webpackConfigString = (newConfig: any) =>
+        util.inspect(newConfig, {
+          depth: Infinity,
+          compact: false,
+        });
+
+      const fileConfigOriginalPath = path.join(
+        snapshotDirectory,
+        "webpack.original.snapshot"
+      );
+
+      const fileConfigMergedPath = path.join(
+        snapshotDirectory,
+        "webpack.merged.snapshot"
+      );
+
+      if (!fs.existsSync(snapshotDirectory)) {
+        fs.mkdirSync(snapshotDirectory);
+      }
+
+      if (options.moduleCallback) {
+        context.logger.info(`\nWrite original config snapshot callback`);
+        fs.writeFileSync(
+          fileConfigOriginalPath,
+          `module.exports = ${webpackConfigString(config)};`
+        );
+
+        const finalConfig = configCallback(config, context);
+
+        context.logger.info(`\nWrite merged config snapshot callback`);
+        fs.writeFileSync(
+          fileConfigMergedPath,
+          `module.exports = ${webpackConfigString(config)};`
+        );
+
+        if (finalConfig) return finalConfig;
+      }
+
+      return config;
+    },
+  });
+}
+
+export default createBuilder<CustomBuilderSchema>(customKarmaBuilder);
